4d31809fb5533f95ef4a5d9b66c3e41a086dea7e
Your Name <you@example.com>
2024-03-28 14:45:26 +0700

    chat-module

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Events\MessageSent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +11,33 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('chat', function () {
+    return view('chat');
+});
+
+Route::post('message', function (Request $request) {
+    broadcast(new MessageSent(auth()->user(), $request->input('message')));
+
+    return $request->input('message');
+});
+
+Route::get('login/{id}', function ($id) {
+    Auth::loginUsingId($id);
+
+    return back();
+});
+
+Route::get('logout', function () {
+    Auth::logout();
+
+    return back();
 });
